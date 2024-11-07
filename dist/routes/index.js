374955341116414f8b7ce2adac3e30e9043d3952
Samuel Adesugba <adeolasam20@gmail.com>
2024-11-07 11:26:54 +0100

    add swagger doc

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootRouter = void 0;
-const express_1 = require("express");
-const auth_1 = require("./auth");
-const admin_1 = require("./admin");
-exports.rootRouter = (0, express_1.Router)();
-exports.rootRouter.use('/auth', auth_1.authRoutes);
-exports.rootRouter.use('/admin', admin_1.adminRoutes);
