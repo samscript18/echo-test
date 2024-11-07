9f9a9bf28f9b349a488e5df64db9e60a8af22b7d
Samuel Adesugba <adeolasam20@gmail.com>
2024-11-07 11:21:46 +0100

    add swagger doc

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import secrets from './constants/secrets.const';
 import { rootRouter } from './routes';
 import { PrismaClient } from '@prisma/client';
 import { errorMiddleware } from './middlewares/error';
+import * as swaggerUI from 'swagger-ui-express';
+import * as YAML from 'yamljs';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use('/api', rootRouter);
+app.get('/', (req, res) => {
+  res.send('<h1>Echo-Test API</h1>');
+});
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export const prismaClient = new PrismaClient({});
 
